75140019146f76f211a96009cc39e34894ddc32a
Felix <felix_reynoso1@ucne.edu.do>
2021-11-07 12:12:44 -0400

    Agregando el Contexto al proyecto.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P2_AP1_Felix_20180570.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Felix_20180570.DAL
+{
+    public class Contexto : DbContext
+    {
+
+        public DbSet<TiposTareas> TiposTareas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\BDProyectos.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TiposTareas>().HasData(new TiposTareas
+            {
+                TipoTareaID = 1,
+                TipoTarea = "Analisis",
+            });
+
+            modelBuilder.Entity<TiposTareas>().HasData(new TiposTareas
+            {
+                TipoTareaID = 2,
+                TipoTarea = "Diseño",
+            });
+
+            modelBuilder.Entity<TiposTareas>().HasData(new TiposTareas
+            {
+                TipoTareaID = 3,
+                TipoTarea = "Programacion",
+            });
+
+            modelBuilder.Entity<TiposTareas>().HasData(new TiposTareas
+            {
+                TipoTareaID = 4,
+                TipoTarea = "Prueba",
+            });
+        }
+
+    }
+}
