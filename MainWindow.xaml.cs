d2c0171fbdaf7d1484754d763ebed55987c91297
Felix <felix_reynoso1@ucne.edu.do>
2021-11-07 12:15:31 -0400

    Agregando preconfiguracion de la base de datos y migraciones del proyecto.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P2_AP1_Felix_20180570.UI.Consultas;
+using P2_AP1_Felix_20180570.UI.Registros;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,26 @@ namespace P2_AP1_Felix_20180570
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+      
+
+        private void ConsultaTiposTareasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            CTiposTareas ctt = new CTiposTareas();
+            ctt.Show();
+        }
+
+        private void RegistroTareasMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            rProyectos rt = new rProyectos();
+            rt.Show();
+        }
+
+        private void ConsultaProyectosMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            cProyectos cp = new cProyectos();
+            cp.Show();
         }
     }
 }
---
+++ Migrations/20211106190404_Inicial.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P2_AP1_Felix_20180570.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TiposTareas",
+                columns: table => new
+                {
+                    TipoTareaID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TipoTarea = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TiposTareas", x => x.TipoTareaID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TiposTareas",
+                columns: new[] { "TipoTareaID", "TipoTarea" },
+                values: new object[] { 1, "Analisis" });
+
+            migrationBuilder.InsertData(
+                table: "TiposTareas",
+                columns: new[] { "TipoTareaID", "TipoTarea" },
+                values: new object[] { 2, "Diseño" });
+
+            migrationBuilder.InsertData(
+                table: "TiposTareas",
+                columns: new[] { "TipoTareaID", "TipoTarea" },
+                values: new object[] { 3, "Programacion" });
+
+            migrationBuilder.InsertData(
+                table: "TiposTareas",
+                columns: new[] { "TipoTareaID", "TipoTarea" },
+                values: new object[] { 4, "Prueba" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TiposTareas");
+        }
+    }
+}
