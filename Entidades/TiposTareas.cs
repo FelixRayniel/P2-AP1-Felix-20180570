6a80c2af579eb50a80031bcdcbd13f2e16302d49
Felix <felix_reynoso1@ucne.edu.do>
2021-11-07 12:11:15 -0400

    Agrenado la entidad TiposTareas al proyecto.

---
+++ Entidades/TiposTareas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Felix_20180570.Entidades
+{
+    public class TiposTareas
+    {
+        [Key]
+        public int TipoTareaID { get; set; }
+        public string TipoTarea { get; set; }
+
+    }
+}
