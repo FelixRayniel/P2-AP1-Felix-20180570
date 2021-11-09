d912435627be82aa1629b90cbbd00aa3ad1a0451
Felix <felix_reynoso1@ucne.edu.do>
2021-11-08 20:20:45 -0400

    Configurando el registro proyecto.

--- UI/Registros/rProyectos.xaml.cs
+++ UI/Registros/rProyectos.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P2_AP1_Felix_20180570.BLL;
+using P2_AP1_Felix_20180570.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,19 +21,58 @@ namespace P2_AP1_Felix_20180570.UI.Registros
     /// </summary>
     public partial class rProyectos : Window
     {
+        private Proyectos proyecto = new Proyectos();
         public rProyectos()
         {
+            this.DataContext = proyecto;
             InitializeComponent();
+            TipoTareaComboBox.ItemsSource = TiposTareasBLL.GetTipoTarea();
+            TipoTareaComboBox.SelectedValuePath = "TipoTareaID";
+            TipoTareaComboBox.DisplayMemberPath = "TipoTarea";
+
+
+
+        }
+
+        public void Limpiar()
+        {
+            this.proyecto = new Proyectos();
+            this.DataContext = proyecto;
+        }
+
+        public void Cargar()
+        {
+            this.DataContext = null;
+            this.DataContext = proyecto;
         }
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
+            Proyectos proyectoEncontrado = ProyectosBLL.Buscar(proyecto.ProyectoID);
+
+            if (proyectoEncontrado != null)
+            {
+                proyecto = proyectoEncontrado;
+                Cargar();
+            } else
+            {
+                Limpiar();
+                MessageBox.Show("NO SE PUEDO ENCONTRAR EL REGISTRO EN LA BASE DE DATOS", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+
 
         }
 
         private void AgregarFilaButton_Click(object sender, RoutedEventArgs e)
         {
+            proyecto.DetalleTarea.Add(new TareasDetalle(
+                (int)TipoTareaComboBox.SelectedValue, RequerimientoTextBox.Text, 
+                int.Parse(TiempoTextBox.Text), (TiposTareas)TipoTareaComboBox.SelectedItem
+                ));
 
+            proyecto.TiempoTotal += int.Parse(TiempoTextBox.Text);
+
+            Cargar();
         }
 
         private void RemoverFilaButton_Click(object sender, RoutedEventArgs e)
@@ -41,17 +82,64 @@ namespace P2_AP1_Felix_20180570.UI.Registros
 
         private void NuevoButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Limpiar();
         }
 
         private void GuardarButton_Click(object sender, RoutedEventArgs e)
         {
+            if (!Validar())
+            {
+                return;
+            }
+            var paso = ProyectosBLL.Guardar(proyecto);
 
+            if (paso)
+            {
+                Limpiar();
+                MessageBox.Show("SE HA GUARDADO EL PROYECTO EXISTOSAMENTE", "Existo", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("NO SE PUDO GUARDAR EL PROYECTO", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
         }
 
         private void EliminarButton_Click(object sender, RoutedEventArgs e)
         {
+            Proyectos proyectoEncontrado = ProyectosBLL.Buscar(proyecto.ProyectoID);
 
+            if (proyectoEncontrado == null)
+            {
+                MessageBox.Show("NO SE PUEDO ENCONTRAR EL REGISTRO EN LA BASE DE DATOS", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            else
+            {
+                ProyectosBLL.Eliminar(proyecto.ProyectoID);
+                MessageBox.Show("PROYECTO ELIMINADO", "Error", MessageBoxButton.OK, MessageBoxImage.Information);
+                Limpiar();
+            }
+
+        }
+
+        private bool Validar()
+        {
+            bool esValido = true;
+
+            if (ProyectoIDTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("INGRESE UN NUMERO DE ID", "Fallo", MessageBoxButton.OK, MessageBoxImage.Warning); 
+            }
+
+            if (DescripcionTextBox.Text.Length == 0)
+            {
+                esValido = false;
+                MessageBox.Show("INGRESE UNA DESCRIPCION", "Fallo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            
+            return esValido;
         }
     }
 }
