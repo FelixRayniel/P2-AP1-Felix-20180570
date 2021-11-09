e252a547dd2ae56c0029dacc98974c43d6e3cf51
Felix <felix_reynoso1@ucne.edu.do>
2021-11-08 20:19:29 -0400

    Agregando las entidades faltantes al proyecto.

---
+++ Entidades/Proyectos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Felix_20180570.Entidades
+{
+   public class Proyectos
+    {
+        [Key]
+        public int ProyectoID { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string Descripcion { get; set; }
+        public int TiempoTotal { get; set; }
+
+        [ForeignKey("ProyectoID")]
+        public List<TareasDetalle> DetalleTarea { get; set; } = new List<TareasDetalle>();
+
+    }
+}
---
+++ Entidades/TareasDetalle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_AP1_Felix_20180570.Entidades
+{
+    public class TareasDetalle
+    {
+        [Key]
+        public int ID { get; set; }
+        public int TipoTareaID { get; set; }
+        public string Requerimiento { get; set; }
+        public int Tiempo { get; set; }
+        
+        [ForeignKey ("TipoTareaID")]
+        public TiposTareas TipoTareas { get; set; }
+
+        public TareasDetalle()
+        {
+            ID = 0;
+            TipoTareaID = 0;
+            Requerimiento = " ";
+            Tiempo = 0;
+            TipoTareas = null;
+
+        }
+
+        public TareasDetalle(int tipoTareaID, string requerimiento, int tiempo, TiposTareas tipo)
+        {
+            ID = 0;
+            TipoTareaID = tipoTareaID;
+            Requerimiento = requerimiento;
+            Tiempo = tiempo;
+            TipoTareas = tipo;
+
+        }
+    }
+}
